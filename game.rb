75b2b360ff1a08a623897d2425beb7f857f7e5d7
theneddlesking <71749344+theneddlesking@users.noreply.github.com>
2023-05-11 11:39:21 +1000

    oop who

--- game.rb
+++ game.rb
@@ -1,28 +1,68 @@
 require_relative 'tilemap.rb'
 require_relative 'editor.rb'
 
-require 'json'
+require 'gosu'
 
-class GameWindow < Gosu::Window
+class PlatformerGame < Gosu::Window
     WIDTH = 640
     HEIGHT = 480
+    TILE_SIZE = 18
+
+    attr_accessor :current_level
     
     def initialize
       super(WIDTH, HEIGHT)
-      @game_editor = Editor.new()
+      @levels = [Level.new(generate_basic_map(36, 27))]
+      @level_number = 1
+      @current_level = @levels[@level_number - 1]
 
-      @map_data = @game_editor.generate_basic_map(20, 10)
-
-      @tile_map = TileMap.new(self, @map_data)
+      # load tiles from tile map image
+      @tiles = Gosu::Image.load_tiles(self, "./images/tiles_packed.png", TILE_SIZE, TILE_SIZE, true)
     end
-    
+
     def draw
-      @tile_map.draw
+      draw_level(@current_level)
+    end
+
+    def draw_level(level)
+      tile_map = level.tile_map
+
+      map_height = tile_map.map_height
+      map_width = tile_map.map_width
+      map_data = tile_map.map_data
+
+      map_height.times do |y|
+        map_width.times do |x|
+          tile_index = map_data[y][x]
+          next if tile_index == 0 # skip empty tiles
+
+          # get the x and y coordinates of the image in the tile set image
+          tile_x = (tile_index - 1) % (@tiles.size / TILE_SIZE)
+          tile_y = (tile_index - 1) / (@tiles.size / TILE_SIZE)
+
+          # draw the image
+          @tiles[tile_x + tile_y * (@tiles.size / TILE_SIZE)].draw(x * TILE_SIZE, y * TILE_SIZE, 0)
+        end
+      end
     end
 end
 
-game_editor = Editor.new()
+# main game loop
 
-game_editor.export_map_data()
+def main
+    # create game
+    game = PlatformerGame.new
 
-GameWindow.new.show
+    # start first level
+    start_level(game.current_level)    
+
+    # render the game
+    game.show
+end
+
+def start_level(level)
+    # temporary export so that progress isn't lost
+    export_map_data(level.map_data)
+end
+
+main
--- tilemap.rb
+++ tilemap.rb
@@ -1,35 +1,15 @@
-require 'gosu'
+module Tiles
+    SKY = 157
+    DIRT = 5
+    GRASS = 23
+end
 
 class TileMap
-  TILE_SIZE = 18
-  
-  def initialize(window, map_data)
-    @window = window
+  attr_accessor :map_data, :map_height, :map_width
 
+  def initialize(map_data)
     @map_data = map_data
-    @map_width = @map_data[0].length
-    @map_height = @map_data.length
-
-    puts(@map_width.to_s + " " + @map_height.to_s  )
-
-    @tiles = Gosu::Image.load_tiles(@window, "./images/tiles_packed.png", TILE_SIZE, TILE_SIZE, true)
-  end
-
-  def update_map_data(map_data)
-    @map_data = map_data
-    @map_width = @map_data[0].length
-    @map_height = @map_data.length
-  end
-  
-  def draw
-    @map_height.times do |y|
-      @map_width.times do |x|
-        tile_index = @map_data[y][x]
-        next if tile_index == 0 # skip empty tiles
-        tile_x = (tile_index - 1) % (@tiles.size / TILE_SIZE)
-        tile_y = (tile_index - 1) / (@tiles.size / TILE_SIZE)
-        @tiles[tile_x + tile_y * (@tiles.size / TILE_SIZE)].draw(x * TILE_SIZE, y * TILE_SIZE, 0)
-      end
-    end
+    @map_height = map_data.length
+    @map_width = map_data[0].length
   end
 end
